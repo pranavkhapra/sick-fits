2958c7db969c31d56cc1d26a1af78ab4d92c98e6
pranavkhapra <pranavkhapra23@gmail.com>
2021-04-23 12:55:39 +0530

    basically commiting the logo styles and all i am bad at styles huhhhh

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,18 +1,50 @@
 import Link from 'next/link';
 import React from 'react';
+import styled from 'styled-components';
 import Nav from './Nav';
 
+const LogoStyles = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+    background: orange;
+  }
+`;
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
 function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
-        <Link href="/">Uzumaki Store</Link>
+        <LogoStyles>
+          <Link href="/">Uzumaki Store</Link>
+        </LogoStyles>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
 
