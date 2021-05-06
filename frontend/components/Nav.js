9da55c796c83d96834e85d5ea18a8b62003fc7e2
pranavkhapra <pranavkhapra23@gmail.com>
2021-05-06 07:17:15 +0530

    animating the cart and the cart count

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,6 +4,7 @@ import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
 import SignOut from './SignOut';
 import { useCart } from '../lib/cartState';
+import CartCount from './CartCount';
 
 function Nav() {
   // the hook to get the user query for the things and all
@@ -21,6 +22,16 @@ function Nav() {
           <SignOut />
           <button type="button" onClick={openCart}>
             My Cart
+            {/* // basically in reduce we take a acc that is tally which //
+            is returned as a single value and that value at each iteration is
+            rembered // returning a single value */}
+            <CartCount
+              count={user.cart.reduce(
+                (tally, cartItem) =>
+                  tally + (cartItem.product ? cartItem.quantity : 0),
+                0
+              )}
+            />
           </button>
         </>
       )}
