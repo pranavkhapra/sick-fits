3f9eb7f87c84cca8bcea67fab5e1e87cf4009b8e
pranavkhapra <pranavkhapra23@gmail.com>
2021-05-07 17:28:52 +0530

    leaving it here

--- backend/access.ts
+++ backend/access.ts
@@ -8,3 +8,28 @@ export function isSignedIn({ session }: ListAccessArgs) {
   // if there is no session return false
   return !!session;
 }
+
+// // now we are going to check if someone haev some specific permission
+// // each permission is a new function
+// export const permissions = {
+//   canManageProducts({ session }) {
+//     return session?.data.role?.canManageProducts;
+//   },
+// };
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+// Permissions check if someone meets a criteria - yes or no.
+export const permissions = {
+  ...generatedPermissions,
+  isAwesome({ session }: ListAccessArgs): boolean {
+    return session?.data.name.includes('wes');
+  },
+};
