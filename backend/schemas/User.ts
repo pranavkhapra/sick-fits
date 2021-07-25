88fe2cc1d1ce46ccb86c1fd3b5c25a9a61eb609e
pranavkhapra <pranavkhapra23@gmail.com>
2021-07-25 16:06:47 +0530

    order roles and all

--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -1,12 +1,25 @@
 import { list } from '@keystone-next/keystone/schema';
 import { text, password, relationship } from '@keystone-next/fields';
-// import { permissionFields } from './fields';
+import { permissions, rules } from '../access';
 
 // named export not export default name export is {}
 export const User = list({
   // access:
+  access: {
+    create: () => true,
+    read: rules.canManageUsers,
+    update: rules.canManageUsers,
+    // only people with the permission can delete themselves!
+    // You can't delete yourself
+    delete: permissions.canManageUsers,
+  },
   // that..... should they be able to see keystone ui or not
   // ui
+  ui: {
+    // hide the backend UI from regular users
+    hideCreate: (args) => !permissions.canManageUsers(args),
+    hideDelete: (args) => !permissions.canManageUsers(args),
+  },
   fields: {
     name: text({
       isRequired: true,
@@ -29,7 +42,13 @@ export const User = list({
     orders: relationship({ ref: 'Order.user', many: true }),
     // todo roles cart and orders of the user
     // because we need the user in the role and the role in the user
-    role: relationship({ ref: 'Role.assignedTo' }),
+    role: relationship({
+      ref: 'Role.assignedTo',
+      access: {
+        create: permissions.canManageUsers,
+        update: permissions.canManageUsers,
+      },
+    }),
     // TODO: add access controls
     products: relationship({
       ref: 'Product.user',
