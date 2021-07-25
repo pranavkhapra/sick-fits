8be7ddbc1c494e9e4e6de1ededd28e07bf623fe7
pranavkhapra <pranavkhapra23@gmail.com>
2021-07-25 15:54:06 +0530

    hidding ui and all

--- backend/schemas/Role.ts
+++ backend/schemas/Role.ts
@@ -1,8 +1,24 @@
 import { text, relationship } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 import { permissionFields } from './fields';
+import { permissions } from '../access';
 
 export const Role = list({
+  //   we need to hide ui of keystone based on person and all
+  // now a normal person can change user privelage but we need to hide that down
+  // limit access controls
+  access: {
+    create: permissions.canManageRoles,
+    read: permissions.canManageRoles,
+    update: permissions.canManageRoles,
+    delete: permissions.canManageRoles,
+  },
+  // but user normal one can still able to see role like he knows there is somethign called role but what is it so we use these for hidding
+  ui: {
+    hideCreate: (args) => !permissions.canManageRoles(args),
+    hideDelete: (args) => !permissions.canManageRoles(args),
+    isHidden: (args) => !permissions.canManageRoles(args),
+  },
   fields: {
     name: text({ isRequired: true }),
     // all of the permission fields of the fields.ts is now here so that we can
