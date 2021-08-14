85357e6eee2852174db8ed86ca323c44c41412ef
pranavkhapra <pranavkhapra23@gmail.com>
2021-08-14 17:07:05 +0530

    adding the production end point

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://uzumaki-store-keystone.herokuapp.com/`;
+export const prodEndpoint = `https://uzumaki-store.herokuapp.com/api/graphql`;
 export const perPage = 2;
 // per page item
