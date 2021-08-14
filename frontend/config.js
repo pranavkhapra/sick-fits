025eb9c06d0bce80f303f22a5090dd1b01d13de2
pranavkhapra <pranavkhapra23@gmail.com>
2021-08-14 18:58:01 +0530

    trying the last time

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `https://uzumaki-store.herokuapp.com/api/graphql`;
-export const prodEndpoint = `https://uzumaki-store.herokuapp.com/api/graphql`;
+export const endpoint = 'https://uzumaki-store.herokuapp.com/api/graphql';
+export const prodEndpoint = 'https://uzumaki-store.herokuapp.com/api/graphql';
 export const perPage = 2;
 // per page item
